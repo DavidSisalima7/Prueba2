9c847f616ff62ccbf10aa61e97fed63568bc942e
DavidSisalima7 <97250207+DavidSisalima7@users.noreply.github.com>
2022-04-24 16:54:34 -0500

    Segunda prueba
    segunda prueba

--- PruebaGit2/src/pruebagit2/PruebaGit2.java
+++ PruebaGit2/src/pruebagit2/PruebaGit2.java
@@ -16,6 +16,7 @@ public class PruebaGit2 {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("Segunda prueba");
     }
     
 }
